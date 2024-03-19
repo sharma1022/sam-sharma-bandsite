e74abf6fd4ed8f557d814158cf96890f9aa8b299
Sam Sharma <samsharma.dev@gmail.com>
2024-03-18 23:28:02 -0700

    Added some html and styling to comments section

---
+++ scripts/index-page.js
@@ -0,0 +1,22 @@
+const comments = [
+  {
+    name: "Victor Pinto",
+    timestamp: "11/02/2023",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Christina Cabrera",
+    timestamp: "10/28/2023",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Isaac Tadesse",
+    timestamp: "10/20/2023",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+const userName = document.querySelector(".comment__input");
+const commentText = document.querySelector(".comment__txt-area");
+
+
