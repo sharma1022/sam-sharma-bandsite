20949b8a90417124ba457431931ab6d9550d76f9
Sam Sharma <samsharma.dev@gmail.com>
2024-04-16 11:50:37 -0700

    added a method for get comments

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -3,4 +3,15 @@ class BandSiteApi{
         this.apiKey = apiKey;
         this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
     }
+
+    async getComments() {
+        try{
+            const response = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`);
+            const comments = response.data;
+
+            return comments;
+        } catch(e){
+            console.log(e);
+        }
+    }
 }
