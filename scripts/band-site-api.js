75f5ca9f668bf897ba973d8c5320c22d999ac152
Sam Sharma <samsharma.dev@gmail.com>
2024-04-16 12:43:00 -0700

    got comments to display

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,4 +1,4 @@
-class BandSiteApi{
+export class BandSiteApi{
     constructor(apiKey){
         this.apiKey = apiKey;
         this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
@@ -6,7 +6,7 @@ class BandSiteApi{
 
     async getComments() {
         try{
-            const response = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`);
+            const response = await axios.get(`${this.baseUrl}comments?api_key=${this.apiKey}`);
             const comments = response.data;
 
             return comments;
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,20 +1,27 @@
-const commentsArr = [
-  {
-    name: "Victor Pinto",
-    timestamp: Date.parse("11/02/2023"),
-    txt: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Christina Cabrera",
-    timestamp: Date.parse("10/28/2023"),
-    txt: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Isaac Tadesse",
-    timestamp: Date.parse("10/20/2023"),
-    txt: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+import {BandSiteApi} from "./band-site-api.js";
+
+// const commentsArr = [
+//   {
+//     name: "Victor Pinto",
+//     timestamp: Date.parse("11/02/2023"),
+//     txt: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   },
+//   {
+//     name: "Christina Cabrera",
+//     timestamp: Date.parse("10/28/2023"),
+//     txt: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//   },
+//   {
+//     name: "Isaac Tadesse",
+//     timestamp: Date.parse("10/20/2023"),
+//     txt: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//   },
+// ];
+
+const api_key = "2fb74758-7ad6-444e-af0b-e32c7e857341";
+const api = new BandSiteApi(api_key);
+
+const comments = await api.getComments();
 
 const userName = document.querySelector(".comment__input");
 const commentText = document.querySelector(".comment__txt-area");
@@ -29,10 +36,37 @@ commentText.addEventListener("click", () => {
   userName.classList.remove("comment__txt-area--error");
 });
 
-const displayComments = () => {
+//got this fuction from StackOverflow. Changed it a bit to to fit the layout of the project
+const timeSince = (date) => {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+};
+
+const displayComment = (comment) => {
   const commentSection = document.querySelector(".comment__bottom");
 
-  commentsArr.forEach((comment) => {
     const commentContainer = document.createElement("div");
     commentContainer.classList.add("comment__bottom-container");
     commentSection.appendChild(commentContainer);
@@ -66,86 +100,63 @@ const displayComments = () => {
     const txt = document.createElement("p");
     txt.classList.add("comment__text");
     rightSection.appendChild(txt);
-    txt.innerHTML = comment.txt;
-  });
+    txt.innerHTML = comment.comment;
 };
 
-window.onload = () => {
-  displayComments();
-};
+const displayComments = comments => {
+  comments.forEach(displayComment);
+}
 
-const form = document.querySelector(".comment__form");
-form.addEventListener("submit", (commentObj) => {
-  commentObj.preventDefault();
-  let newComment = {};
-  newComment.name = commentObj.target.name.value;
-  newComment.timestamp = Date.now();
-  newComment.txt = commentObj.target.comment.value;
 
-  validateInput(newComment);
-  resetInputs();
-  clearComments();
-  displayComments();
-});
+displayComments(comments);
 
-const resetInputs = () => {
-  userName.value = "";
-  commentText.value = "";
-};
 
-//got this fuction from StackOverflow. Changed it a bit to to fit the layout of the project
-const timeSince = (date) => {
-  var seconds = Math.floor((new Date() - date) / 1000);
+// const form = document.querySelector(".comment__form");
+// form.addEventListener("submit", (commentObj) => {
+//   commentObj.preventDefault();
+//   let newComment = {};
+//   newComment.name = commentObj.target.name.value;
+//   newComment.timestamp = Date.now();
+//   newComment.txt = commentObj.target.comment.value;
 
-  var interval = seconds / 31536000;
+//   validateInput(newComment);
+//   resetInputs();
+//   clearComments();
+//   displayComments();
+// });
 
-  if (interval > 1) {
-    return Math.floor(interval) + " years ago";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months ago";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days ago";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours ago";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes ago";
-  }
-  return Math.floor(seconds) + " seconds ago";
-};
+// const resetInputs = () => {
+//   userName.value = "";
+//   commentText.value = "";
+// };
 
-const clearComments = () => {
-  const commentSection = document.querySelector(".comment__bottom");
-  while (commentSection.firstChild) {
-    commentSection.removeChild(commentSection.lastChild);
-  }
-};
 
-const validateInput = (obj) => {
-  if (obj.name === "" && obj.txt === "") {
-    userName.classList.add("comment__input--error");
-    commentText.classList.add("comment__txt-area--error");
-  } else if (obj.name === "") {
-    userName.classList.add("comment__input--error");
-    commentText.classList.remove("comment__txt-area--error");
-  } else if (!isNaN(obj.name)) {
-    userName.classList.add("comment__input--error");
-    commentText.classList.remove("comment__txt-area--error");
-  } else if (obj.txt === "") {
-    commentText.classList.add("comment__txt-area--error");
-    userName.classList.remove("comment__input--error");
-  } else {
-    userName.classList.remove("comment__input--error");
-    commentText.classList.remove("comment__txt-area--error");
-    userName.classList.add("comment__input--active");
-    commentText.classList.add("comment__txt-area--active");
-    commentsArr.unshift(obj);
-  }
-};
+
+// const clearComments = () => {
+//   const commentSection = document.querySelector(".comment__bottom");
+//   while (commentSection.firstChild) {
+//     commentSection.removeChild(commentSection.lastChild);
+//   }
+// };
+
+// const validateInput = (obj) => {
+//   if (obj.name === "" && obj.txt === "") {
+//     userName.classList.add("comment__input--error");
+//     commentText.classList.add("comment__txt-area--error");
+//   } else if (obj.name === "") {
+//     userName.classList.add("comment__input--error");
+//     commentText.classList.remove("comment__txt-area--error");
+//   } else if (!isNaN(obj.name)) {
+//     userName.classList.add("comment__input--error");
+//     commentText.classList.remove("comment__txt-area--error");
+//   } else if (obj.txt === "") {
+//     commentText.classList.add("comment__txt-area--error");
+//     userName.classList.remove("comment__input--error");
+//   } else {
+//     userName.classList.remove("comment__input--error");
+//     commentText.classList.remove("comment__txt-area--error");
+//     userName.classList.add("comment__input--active");
+//     commentText.classList.add("comment__txt-area--active");
+//     commentsArr.unshift(obj);
+//   }
+// };
